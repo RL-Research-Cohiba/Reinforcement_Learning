2e02b530f437b3162bc87d3da7bfd538cc66977f
davestroud <david@davidstroud.me>
2020-05-04 15:34:20 -0500

    clean up if statement

--- Black_Jack/Black_Jack_ds.py
+++ Black_Jack/Black_Jack_ds.py
@@ -11,7 +11,7 @@ def generate_episode_from_limit_stochastic(bj_env):
     episode = []
     state = bj_env.reset() # here, bj_enb is the env instance
     while True:
-        probs = [0.8, 0.2] is state [0] > 18 else [0.2, 0.8]
+        probs = [0.8, 0.2] if state [0] > 18 else [0.2, 0.8]
         action = np.random.choice(np.arange(2), p = probs)
         next_state, reward, done, info = bj_env.step(action)
         episode.append((state, action, reward))
