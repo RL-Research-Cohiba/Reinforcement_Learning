84cf9d0fe489d7f929187a33e874b0f5685acc66
davestroud <david@davidstroud.me>
2020-05-05 13:27:02 -0500

    add more black jack code in vim

--- Black_Jack/Black_Jack_ds.py
+++ Black_Jack/Black_Jack_ds.py
@@ -18,4 +18,15 @@ def generate_episode_from_limit_stochastic(bj_env):
         state = next_state
         if done:
             break
-        return episode 
+        return episode
+
+def mc_prediction_q(env, num_episodes, generate_episode, gamma=1.0):
+    # intialize empty dictionaries of arrays
+    returns_sum = defaultdict(lambda: np.zeros(env.action_space.n))
+    N = defaultdict(lambda: np.zeros(env.action_space.n))
+    Q = defaultdict(lambda:np.zeros(env.action_space.n))
+    # loop over episodes
+    for i_episode in range(1, num_episodes+1):
+        # let us monitor our progress
+        if i_episode % 1000 == 0::
+            print("\rEpisode {}/{}".format((i_episode), num_episodes), end="")
