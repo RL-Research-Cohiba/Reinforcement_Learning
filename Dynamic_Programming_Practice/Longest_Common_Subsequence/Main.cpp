e2e71665fd48a3f499130913fb1e1bf02388d7c7
davestroud <david@davidstroud.me>
2020-05-05 16:36:16 -0500

    clean up longest common subsequence

--- Dynamic_Programming_Practice/Longest_Common_Subsequence/Main.cpp
+++ Dynamic_Programming_Practice/Longest_Common_Subsequence/Main.cpp
@@ -11,14 +11,20 @@ int LCSLength(string X, string Y, int m, int n)
 		return 0;
 
 	// if last character of X and Y matches
-	if (X[m-1] == Y[n-1])
+	if (X[m - 1] == Y[n - 1])
 		return LCSLength(X, Y, m - 1, n - 1) + 1;
 
 	// else if last character of X and Y don't match
-	return max(LCSLength(X, Y, m, n - 1), LCSLength(X, Y, m - 1
+	return max(LCSLength(X, Y, m, n - 1), LCSLength(X, Y, m - 1, n));
 }
 
-
 // Longest Common Subsequence
-//
-	
+int main()
+{
+	string X = "ABCBDAB", Y = "BDCABA";
+
+	cout << "The length of LCS is " <<
+			LCSLength(X, Y, X.length(), Y.length());
+
+	return 0;
+}
