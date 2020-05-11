e7938621cf368685f02d97b93411d062a41c4e03
davestroud <david@davidstroud.me>
2020-05-11 16:34:25 -0500

    add notes to q learning

--- Q-Learning/main.py
+++ Q-Learning/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 transition_probabilities = [ # shape=[s, a, s']
         [[0.7, 0,3, 0.0], [1.0, 0.0, 0.0], [0.8, 0.2, 0.0]],
         [[0.0, 1.0, 0.0], None, [0.0, 0.0, 1.0]],
@@ -12,4 +14,4 @@ possible_actions = [[0, 1, 2], [0, 2], [1]]
 
 Q_values = np.full((3, 3), np.inf) # -np.inf for imposible possible_actions
 for state, actions, in enumerate(possible_actions):
-    Q_values[state, actions] = 0.0 # for all possible actions 
+    Q_values[state, actions] = 0.0 # for all possible actions
