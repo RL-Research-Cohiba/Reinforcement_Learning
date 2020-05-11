b57be6f3906ae59c9a4d678fa2cd8a8a4c719333
davestroud <david@davidstroud.me>
2020-05-11 17:49:39 -0500

    add new code to q learning algo

--- Q-Learning/main.py
+++ Q-Learning/main.py
@@ -15,3 +15,20 @@ possible_actions = [[0, 1, 2], [0, 2], [1]]
 Q_values = np.full((3, 3), np.inf) # -np.inf for imposible possible_actions
 for state, actions, in enumerate(possible_actions):
     Q_values[state, actions] = 0.0 # for all possible actions
+
+gamma = 0.90  # the discount factor
+
+history1 = [] # Not shown in the book (for the figure below)
+for iteration in range(50):
+    Q_prev = Q_values.copy()
+    history1.append(Q_prev) # Not shown
+    for s in range(3):
+        for a in possible_actions[s]:
+            Q_values[s, a] = np.sum([
+                    transition_probabilities[s][a][sp]
+                    * (rewards[s][a][sp] + gamma * np.max(Q_prev[sp]))
+                for sp in range(3)])
+
+history1 = np.array(history1) # Not shown
+
+print(Q_values)
