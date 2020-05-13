29ac1a1de5973e66ee74c5e40d66abe706636ec5
davestroud <david@davidstroud.me>
2020-05-13 08:52:05 -0500

    add lookup to code

--- Dynamic_Programming_Practice/Longest_Common_Substring/main.cpp
+++ Dynamic_Programming_Practice/Longest_Common_Substring/main.cpp
@@ -7,8 +7,13 @@ using namespace std;
 // X[0..m-1] and Y[0..n-1]
 string LCS(string X, string Y, int m, int n)
 {
-  int maxlen = 0; // Stores the max lenght of LCS
+  int maxlen = 0; // Stores the max length of LCS
   int endingIndex = m; // Stores the ending index of LCS in X
+
+  // Lookup[i][j] stores the length of LCS substring
+  // X[0..i-1], Y[0..j-1]
+  int lookup[m + 1][n + 1];
+  
 
 
 
@@ -23,3 +28,5 @@ int main()
 
   return 0;
 }
+
+ 
