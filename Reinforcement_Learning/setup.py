894b4815feeb4d300774ec9ab391ee356541dec8
davestroud <david@davidstroud.me>
2020-05-14 13:50:26 -0500

    make code pep8 compliant

--- Reinforcement_Learning/setup.py
+++ Reinforcement_Learning/setup.py
@@ -1,49 +1,28 @@
 # Python >=3.5 is required
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+import numpy as np
+import keras
+import tensorflow
+import tensorflow as tf
+import sklearn
 import sys
 assert sys.version_info >= (3, 5)
 
-# Scikit-Learn >0.20 is required
-import sklearn
-assert sklearn.__version__ >= - "0.20"
-
-try:
-    # %tensorflow_version only exists in Colab.
-    %tensorflow_version 2.x
-    !apt update & & apt intall - u libpq-dev libsdl1-dev swig xorg-dev xvfb
-    !pip install - q - Y tf-agents-nightly pyvirtualdisplay gym[atari]
-    IS_COLAB = True
-except Exception:
-    IS_COLAB = False
-
-# Tensorflow >2.0 is required
-import tensorflow as tf
-import tensorflow import keras
-assert tf.__version__ >= "2.0"
-
-
-if not tf.config.list_physical_devices('GPU'):
-    print("No GPU was detected. CNNs can be very slow without a GPU.")
-    if IS_COLAB:
-        print("Go to Runtime > Change runtime and select a GPU hardware accelerator.")
 
 # Common imports
-import numpy as np
-import os
-
 # To make this notebook's output stable across runs
 np.random.seed(42)
 tf.random.set_seed(42)
 
 # To plot pretty figures
-%matplotlib inline
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 mpl.rc('axes', labelsize=14)
 mpl.rc('xtick', labelsize=12)
 mpl.rc('ytick', labelsize=12)
 
 # To get smooth animations
-import matplotlib.animation as animation
 mpl.rc('animation', html='jshtml')
 
 # Where to save the figures
@@ -52,7 +31,8 @@ CHAPTER_ID = "rl"
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
 os.makedirs(IMAGES_PATH, exist_ok=True)
 
-def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution= 300):
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
     print("Saving figure", fig_id)
     if tight_layout:
