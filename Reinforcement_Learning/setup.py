b79c6440d7ce111f5ca9f739bf169931a334cfb3
davestroud <david@davidstroud.me>
2020-05-14 13:03:44 -0500

    add savefig function

--- Reinforcement_Learning/setup.py
+++ Reinforcement_Learning/setup.py
@@ -50,4 +50,11 @@ mpl.rc('animation', html='jshtml')
 PROJECT_ROOT_DIR = "."
 CHAPTER_ID = "rl"
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
 
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution= 300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
