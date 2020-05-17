b5802958030dc69fbf0a6b04ace16d58070ba41d
davestroud <david@davidstroud.me>
2020-05-16 21:09:27 -0500

    add simple hard coded policy

--- Reinforcement_Learning/main.py
+++ Reinforcement_Learning/main.py
@@ -55,3 +55,16 @@ env.close()
 def basic_policy(obs):
     angle = obs[2]
     return 0 if angle < 0 else 1
+
+
+totals = []
+for episode in range(500):
+    episode_rewards = 0
+    obs = env.reset()
+    for set in range(200):
+        action = basic_policy(obs)
+        obs, reward, done info = env.step(action)
+        episod_rewards += reward
+        if done:
+            break
+    totals.append(episod_rewards)
