e0e66a622619000823ecaafc4f28cd728c4d9c9f
davestroud <david@davidstroud.me>
2020-05-16 20:05:52 -0500

    working version, sort of

--- Reinforcement_Learning/main.py
+++ Reinforcement_Learning/main.py
@@ -1,14 +1,52 @@
-import main
+# Python >=3.5 is required
 import gym
-gym.logger.set_level(40)  # Explicitly specify dtype as float32
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os
+import numpy as np
+import keras
+import tensorflow
+import tensorflow as tf
+import sklearn
+import sys
+assert sys.version_info >= (3, 5)
+
+# Common imports
+# To make this notebook's output stable across runs
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# To plot pretty figures
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# To get smooth animations
+mpl.rc('animation', html='jshtml')
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "rl"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
 
 env = gym.make("CartPole-v1")
-obs = env.reset()
+observation = env.reset()
 for _ in range(1000):
     env.render()
     action = env.action_space.sample()  # your agent here (this takes random actions)
     observation, reward, done, info = env.step(action)
 
     if done:
-        obs = env.reset()
+        observation = env.reset()
 env.close()
