9bada76e9ed1b10ac8d9122df458277c475693c2
davestroud <david@davidstroud.me>
2021-01-09 20:37:45 -0600

    add new c file

--- Dynamic_Programming/hello_world.c
+++ Dynamic_Programming/hello_world.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+// Variable declaration
+extern int a, b;
+extern int c;
+extern float f;
+
 int main() {
-    printf("Hello, World! \n");
+    /* variable declaration: */
+    int a, b;
+    int c;
+    float f;
+
+    /* actual initialization */
+    a = 10;
+    b = 20;
+
+    c = a + b;
+    printf("value of c: %d \n", c);
+
+    f = 70.0/3.0;
+    printf("value of f: %f \n", f);
 
     return 0;
 }
