6621230a063fac2e51acc02eb38ac5c9118f70bd
dstroud <david@davidstroud.me>
2021-01-21 18:10:57 -0600

    add finance file

--- Dynamic_Programming/LIS_Problem.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    int arr[] = {1, 101, 2, 3, 100, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Sum of maximum sum increasing "
-            "subsequence is %d\n",
-            maxSumIS(arr, n ) );
-    return 0;
-}
