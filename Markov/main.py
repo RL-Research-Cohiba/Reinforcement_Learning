e21e41635762d0b8b5c8f1380e38f1580c4e4ebd
dstroud <david@davidstroud.me>
2021-04-05 17:57:10 -0500

    add new file

--- Markov/main.py
+++ Markov/main.py
@@ -1,10 +1,14 @@
 import numpy as np
     """
-    Takes the transition matrix and runs through each state of the Markov chain for n time steps. When the chain reaches a steady state, returns the transition probabilities and the time step of the convergence.
+    Takes the transition matrix and runs through each state of the Markov
+    chain for n time steps. When the chain reaches a steady state, returns
+    the transition probabilities and the time step of the convergence.
+   
     @params:
     - transition matrix: transition probabilities
     - n: number of time steps to run. default is 10 steps
-    - print_transitions: tells if we want to print the transition matrix at each time step
+    - print_transitions: tells if we want to print the transition matrix at
+      each time step
 """
     
 transition_matrix = np.array([[0.1, 0.4, 0.3, 0.2],
