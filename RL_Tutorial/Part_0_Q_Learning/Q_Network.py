d4d8a234c64f5e30c3f8db1e5345098a3a1993f4
dstroud <david@davidstroud.me>
2021-04-07 15:13:45 -0500

    establish feedforward network

--- RL_Tutorial/Part_0_Q_Learning/Q_Network.py
+++ RL_Tutorial/Part_0_Q_Learning/Q_Network.py
@@ -7,3 +7,15 @@ import matplotlib.pyplot as plt
 env = gym.make('FrozenLake-v0')
 
 tf.reset_default_graph()
+
+#These lines establish the feed-forward part of the network used to choose actions
+inputs1 = tf.placeholder(shape=[1,16],dtype=tf.float32)
+W = tf.Variable(tf.random_uniform([16,4],0,0.01))
+Qout = tf.matmul(inputs1,W)
+predict = tf.argmax(Qout,1)
+
+#Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+nextQ = tf.placeholder(shape=[1,4],dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(nextQ - Qout))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+updateModel = trainer.minimize(loss)
