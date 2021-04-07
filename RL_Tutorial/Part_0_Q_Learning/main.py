50866b5c7da6ba6deefceff426e79b7067147b33
dstroud <david@davidstroud.me>
2021-04-07 13:11:47 -0500

    add Q variable

--- RL_Tutorial/Part_0_Q_Learning/main.py
+++ RL_Tutorial/Part_0_Q_Learning/main.py
@@ -6,9 +6,13 @@ import numpy as np
 env = gym.make("FrozenLake-v0")
 
 # Initialize table with all zeros
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+
+# Set learning parameters
 lr = .8
 y = .95
 num_episodes = 2000
+
 # create lists to contain total rewards and steps per episode
 #jList
 rList = []
@@ -21,7 +25,6 @@ for i in range(num_episodes):
     # The Q-Table Learning algorithm
     while j < 99:
         j += 1
-        # Choose an action by greedily (with noise) picking form Q Table
-        a = np.argmax(Q[s,:] + np.random.randn(1,env.action_
-                                               space.n*(1./(i+1))))
+        # Choose an action by greedily (with noise) picking from Q Table
+        a = np.argmax(Q[s,:]+np.random.randn(1,env.action_space.n*(1./(i+1))))
         
