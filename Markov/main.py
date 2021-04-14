991120c2be5d83df0308de3f1cc304b2c1758932
dstroud <david@davidstroud.me>
2021-04-13 20:45:55 -0500

    finish if else statements

--- Markov/main.py
+++ Markov/main.py
@@ -35,6 +35,15 @@ def run_markov_chain(transition_matrix, n=10, print_transitions=False):
                 print('Markov chain reached steady-state at time-step = '
                       + str(time_step))
                 
+                if not print_transitions:
+                      print(step)
+                      
+                return step
+              else:
+                    step = next_step
+                    
+              return step
+                
     
 transition_matrix = np.array([[0.1, 0.4, 0.3, 0.2],
                               [0.35, 0.1, 0.25, 0.3],
