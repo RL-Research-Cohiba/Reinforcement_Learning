20a28451a0fda6b9099f642a1cbd09e5ba44811e
dstroud <david@davidstroud.me>
2021-04-13 20:04:27 -0500

    add power transition matrix

--- Markov/main.py
+++ Markov/main.py
@@ -35,3 +35,5 @@ transition_matrix = np.array([[0.1, 0.4, 0.3, 0.2],
                               [0.35, 0.1, 0.25, 0.3],
                               [0.4, 0.3, 0.05, 0.25],
                               [0.42, 0.42, 0.08, 0.08]])
+
+power_transition_matrix = run_markov_chain(transition_matrix)
